0a989ffa6b142889cedf228bfae82813f4b5320f
AndrukhivAndriy <79985930+AndrukhivAndriy@users.noreply.github.com>
2023-02-18 07:00:41 -0800

    Update startup.sh

--- k8s/terr_final/startup.sh
+++ k8s/terr_final/startup.sh
@@ -22,10 +22,8 @@ cd inventory/mycluster
 sed -i "s/# node1 ansible_host=95.54.0.12  # ip=10.3.0.1 etcd_member_name=etcd1/node1 ansible_host=$EXIP/" inventory.ini
 sed -i "s/# node2/node1/g" inventory.ini
 sed -i "s/node1 ansible_host=95.54.0.13  # ip=10.3.0.2 etcd_member_name=etcd2/# node1 ansible_host=95.54.0.13  # ip=10.3.0.2 etcd_member_name=etcd2/" inventory.ini
-cd group_vars/k8s_cluster
-sed -i "s/metallb_enabled: false/metallb_enabled: true/" addons.yml
-sed -i "s/# metallb_avoid_buggy_ips: false/metallb_avoid_buggy_ips: true/" addons.yml
-sed -i 's/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/' k8s-cluster.yml
+cd group_vars/k8s_cluster 
+sed -i "s/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/" k8s-cluster.yml
 cd /home/ubuntu/.ssh/
 
 # Generate and Deploy Key Pair for Ansible 
@@ -34,12 +32,23 @@ yes y | ssh-keygen -t rsa -N '' -C 'ubuntu' -f /home/ubuntu/.ssh/id_rsa -q > /de
 echo "#Add key for Ansible" >> authorized_keys
 cat id_rsa.pub >> authorized_keys
 chmod 600 /home/ubuntu/.ssh/id_rsa
-# To find local IP - uncomment next line
-export INIP=`hostname -I | cut -d' ' -f1`
-
 
 # Deploy K8s Cluster via Kubespray
 cd /home/ubuntu/kubespray
 ansible-playbook -i inventory/mycluster/inventory.ini -u ubuntu -b -v --private-key=/home/ubuntu/.ssh/id_rsa cluster.yml
 
+# Install MetalLB
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+wget https://raw.githubusercontent.com/AndrukhivAndriy/DevOps-BaseCamp/main/k8s/metallbconf.yaml
+# To find local IP - uncomment next line
+export INIP=`hostname -I | cut -d' ' -f1`
+sed -i "s/localip/$INIP\/32/" metallbconf.yaml
+kubectl create -f metallbconf.yaml
+
+# Install Helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
